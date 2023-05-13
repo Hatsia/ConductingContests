0911f8f4a9b02c569293f662ab5003f6b74d3d31
Ivan Serdyukov <ivan.serdyukov270@gmail.com>
2023-05-14 01:04:09 +0300

    Fix Layout, add package

--- ConductingContests/Startup.cs
+++ ConductingContests/Startup.cs
@@ -30,7 +30,7 @@ namespace ConductingContests
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
