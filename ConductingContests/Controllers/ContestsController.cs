0f5f9effb7ec9b663e1491c649a0a8b95818349a
Ivan Serdyukov <ivan.serdyukov270@gmail.com>
2023-09-21 23:29:13 +0300

    Add contest controller and views(Delete, Details, Edit, Index)

--- ConductingContests/Controllers/ContestsController.cs
+++ ConductingContests/Controllers/ContestsController.cs
@@ -7,6 +7,7 @@ using ConductingContests.Data;
 using ConductingContests.Models.Entities;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
+using System;
 
 namespace ConductingContests.Controllers
 {
@@ -23,17 +24,53 @@ namespace ConductingContests.Controllers
             _roleManager = roleManager;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> SelectWinner(int contestId, string winnerId)
+        {
+            var currentContest = await _context.Contests.FindAsync(contestId);
+            var winnerUser = await _userManager.FindByIdAsync(winnerId);
+            if (currentContest == null || winnerUser == null)
+            {
+                return NotFound();
+            }
+
+            currentContest.WinnerUserName = winnerUser.UserName;
+            currentContest.Status = StatusContest.End;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
         // GET: Contests
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Contests.Include(c => c.Category).Include(c => c.User);
+            var currentUser = await _userManager.GetUserAsync(User);
+
+            IQueryable<Contest> currentContests;
+
             if (User.IsInRole("Organizer"))
             {
-                var currentUser = await _userManager.GetUserAsync(User);
-                applicationDbContext = _context.Contests.Where(x => x.User == currentUser).Include(c => c.Category).Include(c => c.User);
+                currentContests = _context.Contests.Include(c => c.Category)
+                                                   .Include(c => c.User)
+                                                   .Where(x => x.UserId == currentUser.Id);
+            }
+            else if (User.IsInRole("Basic"))
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                currentContests = _context.Contests.Include(c => c.Category)
+                                                   .Include(c => c.User)
+                                                   .Include(c => c.ParticipationRequest)
+                                                   .Include(x => x.OfferedService);
+            }
+            else
+            {
+                currentContests = _context.Contests.Include(c => c.Category).Include(c => c.User);
             }
 
-            return View(await applicationDbContext.ToListAsync());
+            return View(await currentContests.ToListAsync());
         }
 
         // GET: Contests/Details/5
@@ -45,9 +82,12 @@ namespace ConductingContests.Controllers
             }
 
             var contest = await _context.Contests
+                .Include(c => c.ParticipationRequest).ThenInclude(u => u.User)
+                .Include(c => c.OfferedService)
                 .Include(c => c.Category)
                 .Include(c => c.User)
                 .FirstOrDefaultAsync(m => m.Id == id);
+
             if (contest == null)
             {
                 return NotFound();
@@ -77,7 +117,7 @@ namespace ConductingContests.Controllers
             {
                 ViewData["StartDate"] = "Choose the correct date";
                 ViewData["EndDate"] = "Change the date";
-                ViewData["CategoryId"] = new SelectList(_context.ContestCategories, "Id", "Id", contest.CategoryId);
+                ViewData["CategoryId"] = new SelectList(_context.ContestCategories, "Id", "Name");
                 ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", contest.UserId);
                 
                 return View(contest);
@@ -112,8 +152,18 @@ namespace ConductingContests.Controllers
             {
                 return NotFound();
             }
-            ViewData["CategoryId"] = new SelectList(_context.ContestCategories, "Id", "Id", contest.CategoryId);
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", contest.UserId);
+            ViewData["CategoryId"] = new SelectList(_context.ContestCategories, "Id", "Name");
+
+            // Получение списка значений enum statusContest
+            var statusContestValues = Enum.GetValues(typeof(StatusContest)).Cast<StatusContest>();
+
+            // Создание SelectList из значений enum
+            var statusContestList = new SelectList(statusContestValues);
+
+            // Сохранение SelectList в ViewData
+            ViewData["StatusContestList"] = statusContestList;
+
+
             return View(contest);
         }
 
@@ -122,7 +172,7 @@ namespace ConductingContests.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,StartDate,EndDate,Status,WinnerUserName,UserId,CategoryId")] Contest contest)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,StartDate,EndDate,Status,WinnerUserName,CategoryId")] Contest contest)
         {
             if (id != contest.Id)
             {
@@ -133,7 +183,20 @@ namespace ConductingContests.Controllers
             {
                 try
                 {
-                    _context.Update(contest);
+                    //То что закоменчено, то было для админа(При изменении сущности менялся UserId на админа).
+                    //var currentUser = await _userManager.GetUserAsync(User);
+                    //contest.UserId = currentUser.Id;
+                    var contestDB = _context.Contests.FirstOrDefault(x => x.Id == contest.Id);
+                    contestDB.Id = contest.Id;
+                    contestDB.Title = contest.Title;
+                    contestDB.Description = contest.Description;
+                    contestDB.StartDate = contest.StartDate;
+                    contestDB.EndDate = contest.EndDate;
+                    contestDB.Status = contest.Status;
+                    contestDB.WinnerUserName = contest.WinnerUserName;
+                    contestDB.CategoryId = contest.CategoryId;
+
+                    //_context.Update(contest);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -183,6 +246,19 @@ namespace ConductingContests.Controllers
             _context.Contests.Remove(contest);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDetails(int contestId, string userName) 
+        {
+            var contest = await _context.Contests.FirstOrDefaultAsync(x => x.Id == contestId);
+
+            if (contest.WinnerUserName == userName) 
+            {
+                // return PartialView("Вы победили!");
+            }
+            // вы проиграли
+            return PartialView();
         }
 
         private bool ContestExists(int id)
